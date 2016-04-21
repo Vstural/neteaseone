598c7e60b4dc8d66a8d9b439ed7a0bc15deb8344
unknown <nzelites529@gmail.com>
2016-04-21 15:55:46 +0800

    finding filename

--- ease/spiders/easeSpider.py
+++ ease/spiders/easeSpider.py
@@ -1,5 +1,9 @@
 import scrapy
 
+import re
+
+def 
+
 class easeSpider(scrapy.Spider):
 	name = "ease"
 	allowed_domains = [
@@ -11,6 +15,10 @@ class easeSpider(scrapy.Spider):
 
 	def parse(self,respones):
 		for sel in respones.xpath('//*[@id="goodsList"]/li'):
-			name = sel.xpath('div/p/a/@title').extract()
-			print(name[0])
+			item_name = sel.xpath('div/p/a/@title').extract()[0]
+			file_id = sel.xpath('div/p/a/@href').extract()[0]
+
+			image_url = sel.xpath('div/p/a/@title').extract()#TODO
+			intro = ""
+			print(type(file_id))
 			
